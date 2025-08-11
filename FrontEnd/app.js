8532b602b3a76b0417267a98c2c99a100dbb9570
Jaybee <emmanuelagbo348@gmail.com>
2025-08-11 02:29:17 +0100

    Updates made

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -5,3 +5,18 @@ const priceValue = document.getElementById("priceValue");
 priceRange.addEventListener("input", () => {
   priceValue.textContent = Number(priceRange.value).toLocaleString();
 });
+
+// code to show only selected price range
+const cards = document.querySelectorAll(".card");
+priceRange.addEventListener("input", () => {
+  const selectedValue = priceRange.value;
+
+  cards.forEach((card) => {
+    if (card.dataset.value === selectedValue) {
+      card.style.display = "block";
+      card.style.margin = 'left'
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
