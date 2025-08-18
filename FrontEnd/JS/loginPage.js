32f5bd565d1a9ef107e0e1f218c07299327295c0
Jaybee <emmanuelagbo348@gmail.com>
2025-08-18 20:34:26 +0100

    Modifying everything

---
+++ FrontEnd/JS/loginPage.js
@@ -0,0 +1,20 @@
+// code to switch between student login page and landlord page
+
+const studentBtn = document.getElementById("studentBtn");
+const landlordBtn = document.getElementById("landlordBtn");
+const studentForm = document.getElementById("studentForm");
+const landlordForm = document.getElementById("landlordForm");
+
+studentBtn.addEventListener("click", () => {
+  studentForm.classList.add("active");
+  landlordForm.classList.remove("active");
+  studentBtn.classList.add("active");
+  landlordBtn.classList.remove("active");
+});
+
+landlordBtn.addEventListener("click", () => {
+  landlordForm.classList.add("active");
+  studentForm.classList.remove("active");
+  landlordBtn.classList.add("active");
+  studentBtn.classList.remove("active");
+});
